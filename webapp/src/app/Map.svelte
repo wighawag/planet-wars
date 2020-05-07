fe0844e526450d1b580963bd9ca83cfcee3ac21c
Wighawag <wighawag@gmail.com>
2020-05-07 11:09:02 +0100

    no zoom limitation, imageSmoothingEnabled was reset somehow?

--- webapp/src/app/Map.svelte
+++ webapp/src/app/Map.svelte
@@ -19,7 +19,7 @@
 	
 	let drawOnChange = true;
 
-	const lowZoomOrder = [0.5, 1/3, 0.25, 1/6, 0.125, 0.0625]; // 48 
+	const lowZoomOrder = [0.5, 1/3, 0.25, 1/5, 1/6, 1/7, 1/8, 1/9, 1/10, 1/11, 1/12, 1/13, 1/14, 1/15, 1/16]; // 48 
 	let canvas;
 	let devicePixelRatio;
 	let windowDevicePxelRatio;
